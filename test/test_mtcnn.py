16862a275d0c8c71a4c4ba4a2e759d8ae003d2c4
KAAANG <cunykang@gmail.com>
2023-12-12 15:28:12 +0800

    feat-new-util

--- test/test_mtcnn.py
+++ test/test_mtcnn.py
@@ -8,35 +8,7 @@ r"""
     测试文件, 在此处测试mtcnn的效果
 """
 import cv2
-from mtcnnruntime import MTCNN
-from PIL import Image, ImageDraw
-
-
-def show_bboxes(img, bounding_boxes, facial_landmarks=[]):
-    """Draw bounding boxes and facial landmarks.
-
-    Arguments:
-        img: an instance of PIL.Image.
-        bounding_boxes: a float numpy array of shape [n, 5].
-        facial_landmarks: a float numpy array of shape [n, 10].
-
-    Returns:
-        an instance of PIL.Image.
-    """
-    line_width = 2
-    img_copy = img.copy()
-    if not isinstance(img_copy, Image.Image):
-        img_copy = Image.fromarray(img_copy)
-    draw = ImageDraw.Draw(img_copy)
-
-    for b in bounding_boxes:
-        draw.rectangle([(b[0], b[1]), (b[2], b[3])], outline="red", width=line_width)
-
-    for p in facial_landmarks:
-        for i in range(5):
-            draw.ellipse([(p[i] - 1.0, p[i + 5] - 1.0), (p[i] + 1.0, p[i + 5] + 1.0)], outline="green", width=line_width)
-
-    return img_copy
+from mtcnnruntime import MTCNN, draw_faces
 
 
 mtcnn = MTCNN()
@@ -49,5 +21,5 @@ boxes, landmarks = mtcnn.detect(img)
 print(len(boxes), boxes, landmarks)
 
 # 画出人脸框
-img2 = show_bboxes(img, boxes, landmarks)
+img2 = draw_faces(img, boxes, landmarks)
 print(landmarks)
